020ae6e9ca6e0ee567e92b992a008962216ed5d1
Maik Fleuter <maik.fleuter@codecentric.de>
2021-08-27 16:23:57 +0200

    Add logging

--- cloud-bootstrap-app/src/main/kotlin/de/codecentric/cloudbootstrap/MountainsController.kt
+++ cloud-bootstrap-app/src/main/kotlin/de/codecentric/cloudbootstrap/MountainsController.kt
@@ -2,12 +2,16 @@ package de.codecentric.cloudbootstrap
 
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RestController
+import mu.KotlinLogging
 
 @RestController
 class MountainsController {
 
+    var logger = KotlinLogging.logger {}
+
     @GetMapping("/mountains")
     fun getAll(): List<Mountain> {
+        logger.info { "Received request against '/mountains' endpoint - returning mountains." }
         return allMountains()
     }
 
